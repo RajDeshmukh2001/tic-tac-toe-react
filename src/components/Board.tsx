f471d43f6beafae3a6d1f093fcfcf6ae6599c296
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 15:10:43 +0530

    feat: add function to detect winner and stop game when someone wins

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,7 +6,7 @@ const Board = () => {
     const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
 
     function handleClick(i: number) {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return
         }
         const nextSquares = squares.slice();
@@ -19,8 +19,32 @@ const Board = () => {
         setXIsNext(!xIsNext);
     }
 
+    const calculateWinner = (squares: (string | null)[]) => {
+        const lines = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const winner = calculateWinner(squares);
+
     return (
         <>
+            {
+                winner ?
+                <h4 className="text-xl font-semibold text-green-700">Winner: {winner}</h4>
+                :
+                <h4 className="text-xl font-semibold">Next player: {xIsNext ? "X" : "O"}</h4>
+            }
             <div className="grid grid-cols-3 gap-1">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
