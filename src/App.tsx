e96bfa914ba7366ad9b9dde9af17c5a14acf8b14
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 12:22:13 +0530

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-2xl text-purple-900 font-extrabold'>Tic Tac Toe - React</h1>
+    </>
+  )
+}
+
+export default App
