812c697393a3aa0b11d246ca41925a170d226d5f
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 13:43:56 +0530

    feat: move game state to Board component and pass value as prop to Square component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,20 +1,23 @@
+import { useState } from "react"
 import Square from "./Square"
 
 const Board = () => {
+    const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
+
     return (
         <>
             <div className="grid grid-cols-3 gap-1">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[0]} />
+                <Square value={squares[1]} />
+                <Square value={squares[2]} />
 
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[4]} />
+                <Square value={squares[5]} />
+                <Square value={squares[6]} />
                 
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[7]} />
+                <Square value={squares[8]} />
+                <Square value={squares[9]} />
             </div>
         </>
     )
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react"
+type SquareProps = {
+    value: string | null,
+}
 
-const Square = () => {
-    const [value, setValue] = useState<string | null>(null);
-
-    const handleClick = () => {
-        setValue("X")
-    }
+const Square = ({ value }: SquareProps) => {
 
     return (
         <button
-            className="w-20 h-20 px-4 py-2 text-4xl font-extrabold border border-neutral-600 flex items-center justify-center" 
-            onClick={handleClick}
+            className="w-20 h-20 px-4 py-2 text-4xl font-extrabold border border-neutral-600 flex items-center justify-center"
         >
             {value}
         </button>
