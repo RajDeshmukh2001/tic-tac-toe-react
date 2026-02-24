f1c1d872de465220e9ee1c1cec88ce5444584301
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 13:11:24 +0530

    refactor: add gap between square boxes and change border color

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,17 +1,17 @@
 const Square = () => {
     return (
-        <div className="grid grid-cols-3">
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+        <div className="grid grid-cols-3 gap-1">
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
 
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
 
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
-            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
         </div>
     )
 }
