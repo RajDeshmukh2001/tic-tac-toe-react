7a2265a96766b197b172f99888f9184b7b4c0368
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 13:05:41 +0530

    feat: add tic tac toe square box UI with 3x3 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Square from './components/Square'
 
 function App() {
   return (
-    <>
+    <main className='w-full h-screen flex flex-col items-center justify-center gap-4'>
       <h1 className='text-2xl text-purple-900 font-extrabold'>Tic Tac Toe - React</h1>
-    </>
+
+      <Square />
+    </main>
   )
 }
 
---
+++ src/components/Square.tsx
@@ -0,0 +1,19 @@
+const Square = () => {
+    return (
+        <div className="grid grid-cols-3">
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+            <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-900">✕</button>
+        </div>
+    )
+}
+
+export default Square
