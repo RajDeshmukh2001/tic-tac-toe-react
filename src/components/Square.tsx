b40b4a59e0030734b6569777eee21e2d23d02446
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 13:36:35 +0530

    feat: add click handler to mark square with X

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
 const Square = () => {
+    const [value, setValue] = useState<string | null>(null);
+
+    const handleClick = () => {
+        setValue("X")
+    }
+
     return (
-        <button className="px-4 py-2 text-4xl font-extrabold border border-neutral-600">✕</button>
+        <button
+            className="w-20 h-20 px-4 py-2 text-4xl font-extrabold border border-neutral-600 flex items-center justify-center" 
+            onClick={handleClick}
+        >
+            {value}
+        </button>
     )
 }
 
