393cbb493378b40ead9d9ba618f09d318ebb0f0f
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-24 14:42:43 +0530

    feat: add click handler to update board state on square click

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,12 +1,14 @@
 type SquareProps = {
-    value: string | null,
+    value: string | null;
+    onSquareClick: () => void;
 }
 
-const Square = ({ value }: SquareProps) => {
+const Square = ({ value, onSquareClick }: SquareProps) => {
 
     return (
         <button
             className="w-20 h-20 px-4 py-2 text-4xl font-extrabold border border-neutral-600 flex items-center justify-center"
+            onClick={onSquareClick}
         >
             {value}
         </button>
